ebc6bd76878e829f99ace2f87d69151b3c830e8a
lums <jeremy@lums.io>
2020-10-07 17:35:58 +0200

    Add function for single round (TODO: user input checks)

--- script.js
+++ script.js
@@ -1,8 +1,84 @@
 function computerPlay() {
-    let choices = ["Rock" , "Paper" , "Scissors"];
+    let choices = ["rock" , "paper" , "scissors"];
     let choice = choices[Math.floor(Math.random() * 3)]
     return choice;
 }
 
 //ask player to pick rock paper or scissors
 //play single round
+
+
+function playRound(playerSelection, computerSelection) {
+    // player picks rock
+    const playerPickLowerCase = playerSelection.toLowerCase();
+    switch (playerPickLowerCase) {
+        case "rock":
+            switch (computerSelection) {
+                case "rock":
+                    console.log("Tie! Rock vs rock");
+                    break;
+                case "paper":
+                    console.log("You lose! Paper beats rock");
+                    break;
+                case "scissors":
+                    console.log("You win! Scissors beat rock");
+                default:
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerSelection) {
+                case "rock":
+                    console.log("You win! Paper beats rock");
+                    break;
+                case "paper":
+                    console.log("Tie! Paper vs paper");
+                    break;
+                case "scissors":
+                    console.log("You lose! Scissors beats paper");
+                default:
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerSelection) {
+                case "rock":
+                    console.log("You lose! Rock beats scissors");
+                    break;
+                case "paper":
+                    console.log("You win! Scissors beats paper");
+                    break;
+                case "scissors":
+                    console.log("Tie! Scissors vs scissors");
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+function game() {
+
+    let playerSelection = prompt("enter rock, paper, or scissors");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    
+    let playerSelection = prompt("enter rock, paper, or scissors");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+
+    let playerSelection = prompt("enter rock, paper, or scissors");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+
+    let playerSelection = prompt("enter rock, paper, or scissors");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+
+    let playerSelection = prompt("enter rock, paper, or scissors");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+}
