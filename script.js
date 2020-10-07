6338dda5c8969602cd5722002bb7510f7be57bd7
lums <jeremy@lums.io>
2020-10-07 16:21:04 +0200

    Add base files & rps random function

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    let choices = ["Rock" , "Paper" , "Scissors"];
+    let choice = choices[Math.floor(Math.random() * 3)]
+    return choice;
+}
+
+//ask player to pick rock paper or scissors
+//play single round
